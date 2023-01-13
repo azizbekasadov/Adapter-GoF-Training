03eab127eb1e132417405b38bc41f9dd82e7e5eb
Azizbek Asadov <azizbek1998@icloud.com>
2023-01-13 21:45:49 +0500

    Initial Commit

---
+++ 6. Adapter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  6. Adapter
+//
+//  Created by Azizbek Asadov on 13/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
